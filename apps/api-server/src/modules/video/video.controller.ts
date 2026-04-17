5831fb181280f93acc9331acf16a81b122279299
ShibaTatsuya0905 <letuan09052004@gmail.com>
2026-04-17 17:09:38 +0700

    fix: sửa lỗi kiểu dữ liệu req.user và params id

--- apps/api-server/src/modules/video/video.controller.ts
+++ apps/api-server/src/modules/video/video.controller.ts
@@ -5,6 +5,9 @@ import { catchAsync } from '../../common/utils/catchAsync';
 export class VideoController {
   static getVideos = catchAsync(async (req: Request, res: Response) => res.json(await VideoService.list(req.query)));
   static requestUpload = catchAsync(async (req: Request, res: Response) => res.status(201).json(await VideoService.requestUpload(req.user!.sub, req.body.title, req.body.filename)));
-  static confirm = catchAsync(async (req: Request, res: Response) => res.json(await VideoService.confirmUpload(req.params.id)));
-  static delete = catchAsync(async (req: Request, res: Response) => res.json(await VideoService.delete(req.params.id, req.user!.sub, req.user!.role)));
-}
+  static confirm = catchAsync(async (req: Request, res: Response) => 
+  res.json(await VideoService.confirmUpload(req.params.id as string))
+);
+  static delete = catchAsync(async (req: Request, res: Response) => 
+  res.json(await VideoService.delete(req.params.id as string, req.user!.sub, req.user!.role))
+);}

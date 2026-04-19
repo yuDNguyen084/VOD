2d327665cf5e5e047203094432c8db593726014d
ShibaTatsuya0905 <letuan09052004@gmail.com>
2026-04-19 22:30:08 +0700

    feat: hoàn thiện module worker và fix lỗi type id

--- apps/api-server/src/modules/worker/worker.repository.ts
+++ apps/api-server/src/modules/worker/worker.repository.ts
@@ -1,4 +1,10 @@
 import { prisma } from '../../database';
+import { JobStatus, VideoStatus } from '@prisma/client';
+
 export class WorkerRepository {
-  static updateProgress = (videoId: string, data: any) => prisma.mediaJob.update({ where: { videoId }, data });
+  static updateJob = (jobId: string, data: any) => 
+    prisma.mediaJob.update({ where: { id: jobId }, data });
+
+  static updateVideoStatus = (videoId: string, status: VideoStatus, hlsUrl?: string) =>
+    prisma.video.update({ where: { id: videoId }, data: { status, hlsUrl } });
 }
